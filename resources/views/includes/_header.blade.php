ea59b16a81268aed6b79054f5637153fe904bb6e
Sovana Siswonugroho <vana.hantoro7@gmail.com>
2021-10-19 07:09:45 +0700

    Remove border from navbar toggler

--- resources/views/includes/_header.blade.php
+++ resources/views/includes/_header.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-light navbar-expand-lg bg-white sticky-top">
   <div class="container px-md-2">
     <a href="{{ url('home') }}" class="navbar-brand py-20"><img src="{{ url('images/logo-botolplastik.png') }}" class="brand-md-cs" alt=""></a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler border-0 p-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse justify-content-end align-items-end pb-md-2 mt-md-5 mt-0 pt-0  pt-md-1 pb-0" id="navbarNav">
