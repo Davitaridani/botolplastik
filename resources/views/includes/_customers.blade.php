1450242e8cb1651ea762e350908505ee959369f3
Sovana Siswonugroho <vana.hantoro7@gmail.com>
2021-10-19 19:05:41 +0700

    Replace all customers logo

--- resources/views/includes/_customers.blade.php
+++ resources/views/includes/_customers.blade.php
@@ -32,7 +32,7 @@
       <div class="row g-2 g-lg-3 mx-0 justify-content-between">
         @foreach ($customer as $item)
         <div class="col-md-1 col-6 p-md-0 p-4 d-flex align-items-center justify-content-center">
-          <a href="{{ $item['link'] }}"><img src="{{ url($item['img']) }}" width="100%" alt=""></a>
+          <a href="{{ $item['link'] }}"><img src="{{ url($item['img']) }}" alt="customer" class="img-fluid"></a>
         </div>
         @endforeach
       </div>
